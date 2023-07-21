0601cf3771e8f6b4c7679abaeffe563b284a4a3f
gasparellodev <vinygasparello@gmail.com>
2023-07-21 06:19:11 -0300

    feat: create method for create fake students

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,6 +5,20 @@ const prisma = new PrismaClient();
 
 async function run() {
   await prisma.student.deleteMany();
+
+  const promises = [];
+
+  for (let i = 0; i < 40; i++) {
+    promises.push(
+      prisma.student.create({
+        data: {
+          name: faker.person.fullName(),
+          email: faker.internet.email(),
+          lastAccess: faker.date.past(),
+        },
+      }),
+    )
+  }
 }
 
 run()
